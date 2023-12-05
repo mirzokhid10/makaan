0383379bafcd3aa169b15e1cd1d83b9fd6752a8c
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2023-12-05 20:44:17 +0500

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name'=> 'Admin',
+                'username'=> 'admin',
+                'email'=> 'admin@example.com',
+                'email_verified_at' => Carbon::now(),
+                'password'=> Hash::make('111'),
+                'phone' => random_int(100000000, 999999999),
+                'role'=> 'admin',
+                'status'=> 'active',
+            ],
+            // Agent
+            [
+                'name'=> 'Agent',
+                'username'=> 'agent',
+                'email'=> 'agent@example.com',
+                'email_verified_at' => Carbon::now(),
+                'password'=> Hash::make('111'),
+                'phone' => random_int(100000000, 999999999),
+                'role'=> 'agent',
+                'status'=> 'active',
+            ],
+            // User
+            [
+                'name'=> 'User',
+                'username'=> 'user',
+                'email'=> 'user@example.com',
+                'email_verified_at' => Carbon::now(),
+                'password'=> Hash::make('111'),
+                'phone' => random_int(100000000, 999999999),
+                'role'=> 'user',
+                'status'=> 'active',
+            ],
+        ]);
+    }
+}
---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CodePen - Products Dashboard UI</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="{{ asset('../assets/css/style.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="app-container rounded-0">
+        @include('admin.body.sidebar')
+        <div class="app-content">
+            @include('admin.body.header')
+            @yield('admin')
+        </div>
+    </div>
+
+    <script src="{{ asset('../assets/js/script.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
